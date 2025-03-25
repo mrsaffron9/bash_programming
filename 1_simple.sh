bce296c721cc10b68cb470206076ada013e8cf81
mrsaffron9 <mrsaff1234@gmail.com>
2025-03-25 22:33:59 +0000

    Update 1_simple.sh

--- 1_simple.sh
+++ 1_simple.sh
@@ -2,7 +2,7 @@
  this is the basic program to print names what ever you want by executing ./filename arg1 as name arg2 as city arg3 as streetname and hit on the enter.
 COMMENT
 #!/bin/bash
-echo "this is a script name: $0"
-echo Argument 1: $1
-echo Argument 2: $2
-echo Argument 3: $3
+echo "this is a script name: $file_name"
+echo Argument 1: $argument_name
+echo Argument 2: $argument_city
+echo Argument 3: $argument_street
