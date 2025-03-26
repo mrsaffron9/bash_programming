eea7c48a46ce2be5003f696775c41d6d77c1e1e6
mrsaffron9 <mrsaff1234@gmail.com>
2025-03-26 03:26:16 +0000

    0_Createbasic_usage.sh
    basic

---
+++ basic_usage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "what day is today?
+date
+echo "who logged-in today?"
+who
+echo "what are the files avaliable in current local directory?"
+ls
+echo "reads the files in current local directory.."
+cat *
+echo "reading completed.................."
