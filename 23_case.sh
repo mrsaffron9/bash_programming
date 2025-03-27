4dd004957d2f576242a206bb7a752511031ae68e
mrsaffron9 <mrsaff1234@gmail.com>
2025-03-27 17:25:44 -0400

    case statement

---
+++ 23_case.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+case $USER in bob | max | rackz )
+	echo "welcom $USER"
+	echo "Enjoy the visit ;)"
+	;;
+	testing)
+	echo " testing user"
+	;;
+	*)
+	echo "you're not allowed"
+esac
+
